9ee71e54aefcc7a576dcfcacb137d0515b4b180a
Hugo Demule <hugo.demulerey@gmail.com>
2024-04-09 11:44:04 +0200

    cleaning

--- src/main/java/Classes/Printer.java
+++ src/main/java/Classes/Printer.java
@@ -1,15 +1,4 @@
 package Classes;
 
-import Interfaces.Displayer;
-
-public class Printer implements Displayer<Level>, Displayer<Board> {
-    @Override
-    public void draw(Board object) {
-
-    }
-
-    @Override
-    public void draw(Level object) {
-
-    }
+public class Printer {
 }
