dc1a3c0d5a20b74e618ada179f15f4f6455a6890
Nathan Gromb <nathan.gromb@epfl.ch>
2024-04-29 10:59:23 +0200

    checkpoint propagation

--- src/main/java/Model/Classes/Token/Checkpoint.java
+++ src/main/java/Model/Classes/Token/Checkpoint.java
@@ -4,19 +4,28 @@ import Model.Classes.Utils.Orientation;
 
 import java.util.Set;
 
-public class Checkpoint extends OrientedToken{
+public class Checkpoint extends OrientedToken {
 
-    public Checkpoint(boolean movable, Orientation orientation) {
-       super(movable,orientation);
-    }
+	public Checkpoint(boolean movable, Orientation orientation) {
+		super(movable, orientation);
+	}
 
-    @Override
-    public Set<Orientation> propagateLaser(Orientation orientation) {
-        return Set.of(orientation);
-    }
+	@Override
+	public Set<Orientation> propagateLaser(Orientation orientation) {
+		return switch (getOrientation()) {
+			case UP, DOWN -> switch (orientation) {
+				case UP, DOWN -> Set.of(orientation);
+				default -> Set.of();
+			};
+			case LEFT, RIGHT -> switch (orientation) {
+				case LEFT, RIGHT -> Set.of(orientation);
+				default -> Set.of();
+			};
+		};
+	}
 
-    @Override
-    public Token copy() {
-        return new Checkpoint(this.isMovable(),this.getOrientation());
-    }
+	@Override
+	public Token copy() {
+		return new Checkpoint(this.isMovable(), this.getOrientation());
+	}
 }
