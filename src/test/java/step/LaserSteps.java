2499ac73a0e27cc52460f1b07786055ba6e12fb8
Nathan Gromb <nathan.gromb@epfl.ch>
2024-04-30 11:02:54 +0200

    finished updating outdated tests

--- src/test/java/step/LaserSteps.java
+++ src/test/java/step/LaserSteps.java
@@ -11,23 +11,25 @@ import io.cucumber.java.en.When;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LaserSteps {
-    private Level level;
-    private Laser laser;
+	private Level level;
+	private Laser laser;
 
-    @Given("To test LaserGenerator, I have the level with ID {string}")
-    public void toTestLaserGeneratorIHaveTheLevelWithID(String arg0) {
-        LevelID levelID = new LevelID(arg0);
-        LevelBuilder levelBuilder = new LevelBuilder(levelID);
-        level = levelBuilder.build();
-    }
+	@Given("To test LaserGenerator, I have the level with ID {string}")
+	public void toTestLaserGeneratorIHaveTheLevelWithID(String arg0) {
+		LevelID levelID = new LevelID(arg0);
+		LevelBuilder levelBuilder = new LevelBuilder(levelID);
+		level = levelBuilder.build();
+	}
 
-    @When("I run generateLaser")
-    public void iRungenerateFirstLaserFragment() {
-        laser = level.laserManager().generateLaser();
-    }
+	@When("I run generateLaser")
+	public void iRungenerateFirstLaserFragment() {
+		assert level != null;
 
-    @Then("{int} LaserFragment must be generated")
-    public void laserfragmentMustBeGenerated(int arg0) {
-        assertEquals(laser.getFragments().size(), arg0);
-    }
+		laser = level.laserManager().generateLaser();
+	}
+
+	@Then("{int} LaserFragment must be generated")
+	public void laserfragmentMustBeGenerated(int arg0) {
+		assertEquals(arg0, laser.getFragments().size());
+	}
 }
